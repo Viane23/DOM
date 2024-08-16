7ebb88ed86cd38d21f00aaa52bfc7506cb7b8535
viane23 <hollandix1991@gmail.com>
2024-08-16 12:47:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+/* 
+let variableName = document.property(arguments);
+*/
+
+let bodyById = document.getElementById('body');
+console.log(bodyById);
+
+let bodyByQ = document.querySelector('body');
+
+let nav = document.querySelector('nav');
+
+searchButton.addEventListener('click', function(){
+    const searchButton = document.getElementById
+}
+
+console.log(nav);
+// find a table element with id="myTable"
+
+var table = document.getElementById('myTable');
+
+var cell1 = HTMLTableRowElement('ten');
+
+var table = getElementById('myTable');
+
+var row = table.insertRow(5);
+var row = table.insertCell(5);
